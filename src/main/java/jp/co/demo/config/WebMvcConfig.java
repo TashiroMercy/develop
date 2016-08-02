fea0be1728ee5d550b20c444dd2af3201446d0fd
TashiroMercy <tashi69akamercy@gmail.com>
2016-08-02 15:42:00 +0900

    no Comment

--- src/main/java/jp/co/demo/config/WebMvcConfig.java
+++ src/main/java/jp/co/demo/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package jp.co.demo.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.resource.GzipResourceResolver;
 
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
@@ -10,6 +11,8 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
     registry.addResourceHandler("/webjars/**")
-    		.addResourceLocations("classpath:/META-INF/resources/webjars/");
+    		.addResourceLocations("classpath:/META-INF/resources/webjars/")
+            .resourceChain(false) // 自動でWebJarsResourceResolverが有効化される
+            ;
   }
 }
