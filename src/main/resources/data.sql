e3013de841334192e763f7ca8bca3e7f9ae547b9
TashiroMercy <tashi69akamercy@gmail.com>
2016-08-03 14:24:08 +0900

    no Comment

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO book VALUES(1,'よくわかるSpring',2000);
+INSERT INTO book VALUES(2,'すぐわかるSpring',2980);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,11 @@
-create table if not exists customer (
+create table customer (
     id int primary key auto_increment,
     name varchar(50),
     email varchar(50)
+);
+
+CREATE TABLE book(
+  book_id INTEGER PRIMARY KEY,
+  book_name VARCHAR(20),
+  price INTEGER
 );
